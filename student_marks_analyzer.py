b3f640852e8cd1c3e05c8f59d516620b824fb934
pravallika-v <pravallikavuyyuru10@gmail.com>
2026-05-15 10:49:16 +0530

    Add files via upload
    Uploaded Python project for calculating total and average marks using lists and loops.

---
+++ student_marks_analyzer.py
@@ -0,0 +1,12 @@
+marks = [45, 67, 89, 90]
+
+total = 0
+
+for i in marks:
+    total = total + i
+    
+average = total / len(marks)
+
+print("Marks:", marks)
+print("Total marks:", total)
+print("Average marks:", average)
